d04becab8c562aa2b7634edf85a72e9e87f79b99
fundagungoralkan <fundaalkan112@gmail.com>
2023-11-03 20:52:52 +0000

    test2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import {useState,useEffect} from "react"
 import './App.css';
 
+import SearchBox from "./components/search-box/search-box.component";
+
 function App() {
+const[monsters,setMonsters]=useState([])          // monster kartlari icin usestate
+const[searchField,setSearchField]=useState("")  // arama sonuclari icin useState
+
+useEffect(()=>{
+  fetch("https://jsonplaceholder.typicode.com/users")     // API bilgilerinin oldugu URL'i fetch ettik
+  .then((res)=>res.json())
+   .then((users)=>setMonsters(users))           // json icindeki kullanicilari useState olan SetMonster'a ekledik
+},[])
+
+const onSearchChange=(e)=>{     //search islemindeki yaptigimiz aramalari lowercase-kucuk harf formatina cevirme 
+const searchFieldString=e.target.value.toLocaleLowerCase()
+setSearchField(searchFieldString)
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className='app-title'>Monsters Rolodex</h1>
+
+   <SearchBox  className="monster-search-box"
+    placeholder="Search Monsters" 
+   onChangeHandler={onSearchChange} />
     </div>
   );
 }
 
 export default App;
+
+// state  = tutma islemi yapar- kargo kolisi
+// props  = tasima islemi yapar- kargo araci 
